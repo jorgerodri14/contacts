73acfc0c2b7dca561e8d524ff59dff14957fe6f8
Jorge Rodríguez Sánchez <jorosadev@gmail.com>
2021-04-14 00:34:41 +0100

    fix url

--- contact_list_api/data/downloadContacts.js
+++ contact_list_api/data/downloadContacts.js
@@ -2,15 +2,14 @@ const fs = require('fs')
 const https = require('https')
 const chalk = require('chalk')
 
-https.get('https://exercise.goldenspear.com/contacts.json', (res) => {
+https.get('https://raw.githubusercontent.com/wellingtoncosta/fake-contacts-api/master/db.json', (res) => {
   let rawData = ''
   res.on('data', (chunk) => {
     rawData += chunk
-    console.log(chalk.green('Chunk capture'))
   });
   res.on('end', () => {
-    const parsedData = rawData.toString()
-    fs.writeFile('./data/contacts.json', parsedData, (error) => {
+    const { contacts } = JSON.parse(rawData)
+    fs.writeFile('./data/contacts.json', JSON.stringify(contacts), (error) => {
       if (error) console.log(chalk.red(err))
       console.log(chalk.inverse.green.bold('Download Success!!'))
     })
